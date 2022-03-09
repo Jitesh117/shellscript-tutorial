9cdd778cf3f8900d850001e311f314b00d472297
Jitesh117 <sahookumarjitesh@gmail.com>
2022-03-09 17:04:00 +0530

    added some more files

---
+++ practice/filename.sh
@@ -0,0 +1,12 @@
+# Write a shell script program to read the name 
+# of a folder and a character. Print all its files 
+# and subfolders along with their path which have 
+# that character in their names . Finally print the
+#  total count.
+echo "Enter the folder name"
+read folder
+echo "Enter the character"
+read char
+cd $folder
+ls *[$char]*
+grep -Rl "curl" ./ -c
