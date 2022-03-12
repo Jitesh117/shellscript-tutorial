4180701da806f689648b251ba6826ac1a752faaa
Jitesh117 <sahookumarjitesh@gmail.com>
2022-03-12 14:08:00 +0530

    done with the first problem

---
+++ week 3/samefile.sh
@@ -0,0 +1,14 @@
+echo "Enter 2 file names:"
+echo "Enter first file"
+read file1
+echo "Enter second file"
+read file2
+
+
+if cmp -s "$file1" "$file2"
+then
+    echo "File match"
+    rm $file2
+else
+    echo "FIle don't match"
+fi
